b28893471d0094545890eb6adf17ec38f2c88c2a
EvaHauthal <eva.hauthal@tu-dresden.de>
2022-08-17 08:57:47 +0200

    Eva's commit 2

--- app/src/main/java/com/example/stupidity/MainActivity.java
+++ app/src/main/java/com/example/stupidity/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // WILL IT WORK?!?
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
